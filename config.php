ee7ddf46323b993c9f1b361b7950c77e833159e2
riccardo <riccardotesta@titansolution.it>
2018-09-18 17:49:26 +0200

    Anaomized data

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * */
 
 
-$conf_comune="Comune di Bistagno";
+$conf_comune="TEST General";
 $conf_logo="/loghi/logo-com.png";
 $conf_data_publicazione="22/01/2018";
 //
@@ -12,12 +12,12 @@ $conf_data_stoppublicazione= date("d/m/y",strtotime("+".$days_end." days"));
 
 //Menu links
 
-$conf_links=[["Comune di Acqui Terme","www.comune.acquiterme.al.it"],
-    ["Gare d'appalto Telematiche","https://appalti-acquese.maggiolicloud.it/PortaleAppalti/it/homepage.wp"]];
+$conf_links=[["linka","http://www.google.it"],
+    ["linkb","https://www.ansa.it"]];
 
 //Header
-$conf_header="CUC";
-$conf_main_content="COMUNE DI BISTAGNO: PROCEDURA NEGOZIATA RELATIVA AL 'CONSOLIDAMENTO RIPA SOTTOSTANTE VIA ADUA' EVENTO ALLUVIONALE NOVEMBRE 2016 ORDINANZA N.430 DEL 10/01/2017 ORDINANZA COMMISSARIALE N.6/A18.000/430 DEL 27/06/2017. CUP F84J17000300005 E CIG 755251006B.";
+$conf_header="HEADER !";
+$conf_main_content="Can you write content or HTML code here!!!";
 
 $folders=["DOCUMENTS","OTHERS"];
 ?>
